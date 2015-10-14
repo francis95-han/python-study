677e4c19fbf816a7b64924bd667be9a6486a2980
ice1995 <mir2285@outlook.com>
2015-10-14 16:30:07 +0800

    2015.10.14
    16:30

--- 0_expetion.py
+++ 0_expetion.py
@@ -2,10 +2,11 @@
 __author__ = 'bohan'
 def reafFloat(requestMsg,errorMsg):
     while True:
-        val = raw_input(requestMsg)
+        val =raw_input(requestMsg)
         try:
             val = float(val)
             return val
         except:
             print(errorMsg)
+
 reafFloat(1,'a')
--- file.py
+++ file.py
@@ -6,7 +6,7 @@ filename = raw_input('Enter filename:')
 try:
     f = open(filename, 'r')
     for eachline in f:
-        print eachline,
+        print eachline
     f.close()
 except IOError:
     print 'couldn\'t open '+filename
