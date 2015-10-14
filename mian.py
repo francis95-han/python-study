f65d6dc632354ff359bf6eae232e641d1b9737ed
ice1995 <mir2285@outlook.com>
2015-10-14 19:45:30 +0800

    2015.10.14 19:45

--- mian.py
+++ mian.py
@@ -1,11 +1,12 @@
-#coding=utf-8
-#!/usr/bin/env python
-__author__ = 'bohan'
+# coding:utf-8
+
+
 def And():
     a = int(raw_input('请输入一个数'))
     b = int(raw_input('请输入另一个数'))
-    c =a*b
+    c = a * b
     print(c)
     return c
-And()
 
+
+And()
