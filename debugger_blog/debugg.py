10753f39f7791e1c7e390141e9351d6ae36069eb
ice1995 <mir2285@outlook.com>
2018-04-26 18:50:16 +0800

    2018/4/26

--- debugger_blog/debugg.py
+++ debugger_blog/debugg.py
@@ -1,5 +1,37 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-""" @author zhangbohan.dell@gmail.com
-    @function:
-    @create 2018/4/21 14:03"""
+# @Date    : 2016-08-11 16:48:16
+# @Author  : giantbranch (giantbranch@gmail.com)
+# @Link    : http://blog.csdn.net/u012763794?viewmode=contents
+
+from ctypes import *
+from debugger_blog.debugger_defines import *
+
+kernel32 = windll.kernel32
+
+
+class debugger():
+
+    def __init__(self):
+        pass
+
+    def load(self, path_to_exe):
+        creation_flags = DEBUG_PROCESS
+        startupinfo = STARTUPINFO()
+        process_information = PROCESS_INFORMATION()
+        startupinfo.dwFlags = 0x1
+        startupinfo.wShowWindow = 0x0
+        startupinfo.cb = sizeof(startupinfo)
+        if kernel32.CreateProcessA(path_to_exe,
+                                   None,
+                                   None,
+                                   None,
+                                   False,
+                                   creation_flags,
+                                   None,
+                                   None,
+                                   byref(startupinfo),
+                                   byref(process_information)):
+            print("[*] we have successfully launched the process!")
+            print("[*] PID:%d" % process_information.dwProcessId)
+        else:
+            print("[*] Error:0x%08x." % kernel32.GetLastError())
--- debugger_blog/test.py
+++ debugger_blog/test.py
@@ -1,5 +1,9 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-""" @author zhangbohan.dell@gmail.com
-    @function:
-    @create 2018/4/21 14:04"""
+# @Date    : 2016-08-12 14:18:10
+# @Author  : giantbranch (giantbranch@gmail.com)
+# @Link    : http://blog.csdn.net/u012763794?viewmode=contents
+
+import debugger_blog.debugg
+
+debugger =debugger_blog.debugg.debugger()
+debugger.load("C:\\WINDOWS\\system32\\calc.exe")
